2247ff3e73c454b6d399290476f24ccd1363f021
Segundo Tipanquiza <segutipanquiza@gmail.com>
2026-01-23 01:16:55 -0500

    feat: User-Serverce microservices

--- services/user-service/src/middleware/rateLimit.middleware.ts
+++ services/user-service/src/middleware/rateLimit.middleware.ts
@@ -1,8 +1,9 @@
-import rateLimit from "express-rate-limit";
+import rateLimit from 'express-rate-limit';
 
-export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
+export const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  limit: 100,
+  message: 'Demasiadas solicitudes desde esta IP, intenta de nuevo más tarde',
+  standardHeaders: 'draft-7', // o 'draft-6' o true
   legacyHeaders: false,
 });
